7ee2bff1a0f4ef338bbf96d6d83bdb3f804d67db
amierfaudzi <amier.faudzi@gmail.com>
2021-03-15 15:20:00 -0400

    add useQuery to get users firstname and lastname

--- src/App.js
+++ src/App.js
@@ -7,9 +7,21 @@ import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Resources from './pages/Resources/Resources';
 import Guides from './pages/Guides/Guides';
+import { gql, useQuery } from '@apollo/client';
 
+const USER_QUERY = gql`
+{
+  users{
+    firstName
+    lastName
+  }
+}`
 
 function App() {
+
+  const {loading, error, data} = useQuery(USER_QUERY, {});
+
+  console.log(data, loading, error)
   return (
     <Router>
       <Navbar/>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+
+const link = createHttpLink({
+  uri: 'https://alejo-backend.herokuapp.com/'
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
