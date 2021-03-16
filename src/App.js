0a1984832d70ce886a837fa2446363ba35ef9198
amierfaudzi <amier.faudzi@gmail.com>
2021-03-16 11:16:43 -0400

    add login query to the app (temporary placement)

--- src/App.js
+++ src/App.js
@@ -7,21 +7,45 @@ import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Resources from './pages/Resources/Resources';
 import Guides from './pages/Guides/Guides';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
 
 const USER_QUERY = gql`
-{
+query {
   users{
     firstName
     lastName
   }
 }`
 
+const exampleUser = {
+  email: "xi.bowving@mail.com",
+  password: "abc123"
+}
+
+const USER_LOGIN = gql`
+query Login($email: String! $password: String!){
+    login(loginInput: {email: $email, password: $password}){
+        token,
+        user {
+          firstName,
+          about
+        }
+    }
+}`
+
 function App() {
 
-  const {loading, error, data} = useQuery(USER_QUERY, {});
+  // const {loading, error, data} = useQuery(USER_QUERY, {});
+  const {loading, error, data} = useQuery(USER_LOGIN, {
+    variables: {
+    email: "xi.bowving@mail.com",
+    password: "abc123"
+  }});
 
-  console.log(data, loading, error)
+  if(data){
+    console.log(data.login, loading, error);
+  }
+
   return (
     <Router>
       <Navbar/>
