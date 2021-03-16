698ea7d24849b93ee3c7e544d406012e0cfbcb69
amierfaudzi <amier.faudzi@gmail.com>
2021-03-16 12:10:00 -0400

    update Home content and styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import Login from '../../components/Login/Login'
+import React from 'react';
+import './Home.scss';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome to Alejo</h1>
-            <h2>Hero banner</h2>
-            <h2>Some guides</h2>
-            <Login/>
+        <div className="home">
+            <div className="hero">
+                <div className="hero__content">
+                    <h1 className="hero__content-title">Welcome to Alejo</h1>
+                    <p className="hero__content-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, recusandae officiis. Mollitia porro repellat vel asperiores deleniti voluptates commodi, veritatis amet necessitatibus ipsum nulla earum, ipsa expedita. Possimus beatae, esse mollitia incidunt necessitatibus, reprehenderit voluptates blanditiis sequi soluta at nisi ratione harum maxime aliquid fugiat perspiciatis error laborum. Recusandae, alias?</p>
+                </div>
+                <div className="hero__image">
+                    <img src="http://placekitten.com/400/480" alt=""/>
+                </div>
+            </div>
+
         </div>
     )
 }
