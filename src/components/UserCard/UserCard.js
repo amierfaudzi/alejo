0cf358675583b84ec6ba9eb7f16925ecc11452fe
amierfaudzi <amier.faudzi@gmail.com>
2021-03-17 00:47:14 -0400

    add overlay to the UserCard component to display more information

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -15,10 +15,15 @@ export default function UserCard() {
             }
             }}>
             <div className="user-card">
-                <img src="http://placekitten.com/300/200" alt="" className="user-card__image"/>
-                <div className="user-card__info">
-                    <h2>John Doe</h2>
-                    <h3>Toronto, Ontario</h3>
+                <div className="user-card__main">
+                    <img src="http://placekitten.com/300/200" alt="" className="user-card__image"/>
+                    <div className="user-card__info">
+                        <h2>John Doe</h2>
+                        <h3>Toronto, Ontario</h3>
+                    </div>
+                </div>
+                <div className="user-card__side">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium fuga similique iusto. Nemo repellat similique molestias?</p>
                 </div>
             </div>
         </Link>
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.scss';
+
+export default function Login() {
+    return (
+        <div className="login">
+            <div className="sign-in">
+                <form>
+                    <h4>Sign in</h4>
+                    <button>Sign In</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+
+// Signup will add open up a modal with the window and while
