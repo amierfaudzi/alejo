fa51048327f273f43e2c7fdfc381dddbc3fff867
amierfaudzi <amier.faudzi@gmail.com>
2021-03-17 13:53:40 -0400

    add mock data and compoent to the Resources page

--- src/pages/Resources/Resources.js
+++ src/pages/Resources/Resources.js
@@ -1,16 +1,76 @@
-import React from 'react'
+import React from 'react';
+import './Resources.scss';
 
 export default function Resources() {
     return (
-        <div>
-            This is the Resources. It will contains
-            <ul>
-                <li>Common Services - Police, Fire, Health, Shopping</li>
-                <li>Transit system</li>
-                <li>*Immigration</li>
-                <li>*Accommodation</li>
-                <li>*Community forum</li>
-            </ul>
+        <div className="resources">
+            <div className="welcome">
+                <h1>Welcome, Oluwakemi</h1>
+            </div>
+            <div className="resources__tray">
+                <div className="resource-card">
+                    <img src="http://placekitten.com/240/135" alt="" className="resource-card__image"/>
+                    <div className="resource-card__content">
+                        <p>Find Jobs</p>
+                    </div>
+                </div>
+                <div className="resource-card">
+                    <img src="http://placekitten.com/240/135" alt="" className="resource-card__image"/>
+                    <div className="resource-card__content">
+                        <p>Find Jobs</p>
+                    </div>
+                </div>
+                <div className="resource-card">
+                    <img src="http://placekitten.com/240/135" alt="" className="resource-card__image"/>
+                    <div className="resource-card__content">
+                        <p>Find Jobs</p>
+                    </div>
+                </div>
+                <div className="resource-card">
+                    <img src="http://placekitten.com/240/135" alt="" className="resource-card__image"/>
+                    <div className="resource-card__content">
+                        <p>Find Jobs</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="post-form">
+                    <img src="http://placekitten.com/80/80" alt="" className="post-form__image"/>
+                    <div className="post-form__container">
+                        <p className="post-form__user">Oluwakemi Adeleke</p>
+                        <form className='form'>
+                            <div>
+                                <label htmlFor="form-content"></label>
+                                <input type="text" placeholder="Ask a question or share a post" id="form-content"/>
+                            </div>
+                            <button className="btn" type="submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+                <div className="community-post">
+                    <div className="post">
+                        <img src="http://placekitten.com/80/80" alt="" className="post__image"/>
+                        <div className="post__content">
+                            <p className="post__poster">Amier Faudzi</p>
+                            <p>Where is the best boba place in Montreal?</p>
+                        </div>
+                    </div>
+                    <div className="post">
+                        <img src="http://placekitten.com/80/80" alt="" className="post__image"/>
+                        <div className="post__content">
+                            <p className="post__poster">Amier Faudzi</p>
+                            <p>Where is the best boba place in Montreal?</p>
+                        </div>
+                    </div>
+                    <div className="post">
+                        <img src="http://placekitten.com/80/80" alt="" className="post__image"/>
+                        <div className="post__content">
+                            <p className="post__poster">Amier Faudzi</p>
+                            <p>Where is the best boba place in Montreal?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
